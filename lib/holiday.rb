f97d8e4be59f0f8480bfaf2bdb9257a891641476
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-29 23:59:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  supply_array = []
   holiday_hash.collect do |key, value|
     if value == "BBQ"
+      supply_array.push(value)
     
 end
 
