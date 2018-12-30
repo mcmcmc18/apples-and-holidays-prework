d5356111099bfe23dfc2cf9d8b7210c0d7a6aac4
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 00:50:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,18 +47,15 @@ holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-     data.each do |holiday, supply|
-       array = holiday.to_s.split(" ")
-       final_holiday = []
-         array.each do |x|
-           final_holiday << x.capitalize!
-         end
-       holiday = final_holiday.join(" ")
- 
-        supply = supply.join(", ")
-       puts "  #{holiday}: #{supply}"
-    end
-   end
+    holiday_hash.each do | season, holiday|
+     puts "#{season.capitalize}:"
+     holiday.each do |holiday, item|
+       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+     end
+  end 
+end
+
+all_supplies_in_holidays(holiday_supplies)
 
 
 def all_holidays_with_bbq(holiday_hash)
